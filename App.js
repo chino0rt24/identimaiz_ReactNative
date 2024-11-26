8cdc744c158eadec9a5fd3ed3a52850d43dc463b
chino0rt24 <ortegapablo0224@gmial.com>
2024-11-25 22:25:34 -0600

    Guardando cambios antes del pull

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { SafeAreaView, Text, StyleSheet } from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>¡Bienvenido a Identimaíz!</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f3f4f6',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+});
+
+export default App;
